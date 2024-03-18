55c6edf66d2191dd439e5871537454262c24b36f
Gleb <gleb.evdokimov.1@gmail.com>
2024-03-18 21:36:00 +0300

    add v-if, v-for

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const vm = Vue.createApp({
       trackImage:
         "https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages122/v4/2c/7f/23/2c7f2312-079a-3d75-c3b5-3df23ac7ca0c/6c0d93c9-0225-4138-85e4-437caca710bc_file_cropped.png/375x375bb.jpg",
       isLike: false,
+      blockWidth: 1000,
+      similarTracks: [
+        {
+          name: "Спи",
+          duration: "3.55",
+        },
+        {
+          name: "Океаны",
+          duration: "2.59",
+        },
+        {
+          name: "Там где",
+          duration: "3.22",
+        },
+      ],
     };
   },
   methods: {
-    like(event) {
-      const icon = event.target.parentElement;
-      icon.classList.remove("text-white");
-      icon.classList.add("text-red-500");
-      this.isLike = true;
+    addLike() {
+      !this.isLike ? (this.isLike = true) : (this.isLike = false);
     },
   },
   computed: {
@@ -22,10 +34,10 @@ const vm = Vue.createApp({
     },
   },
   watch: {
-    isLike() {
-      setTimeout(() => {
-        alert(`Вы поставили отметку - "нравится" на треке "${this.fullName}"`);
-      }, 1000);
-    },
+    // isLike() {
+    //   setTimeout(() => {
+    //     alert(`Вы поставили отметку - "нравится" на треке "${this.fullName}"`);
+    //   }, 1000);
+    // },
   },
 }).mount("#app");
