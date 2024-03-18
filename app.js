582864868bedc6c2a4d1ce538c813cf87cc1c928
Gleb <gleb.evdokimov.1@gmail.com>
2024-03-18 16:42:50 +0300

    create track item ui

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const vm = Vue.createApp({
   data() {
     return {
-      msg: "hello",
+      author: "Луверанс",
+      track: "Душа моя",
+      trackImage:
+        "https://is1-ssl.mzstatic.com/image/thumb/AMCArtistImages122/v4/2c/7f/23/2c7f2312-079a-3d75-c3b5-3df23ac7ca0c/6c0d93c9-0225-4138-85e4-437caca710bc_file_cropped.png/375x375bb.jpg",
+      isLike: false,
     };
   },
-  methods: {},
-  computed: {},
-  watch: {},
+  methods: {
+    like(event) {
+      const icon = event.target.parentElement;
+      icon.classList.remove("text-white");
+      icon.classList.add("text-red-500");
+      this.isLike = true;
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.author} - ${this.track}`;
+    },
+  },
+  watch: {
+    isLike() {
+      setTimeout(() => {
+        alert(`Вы поставили отметку - "нравится" на треке "${this.fullName}"`);
+      }, 1000);
+    },
+  },
 }).mount("#app");
